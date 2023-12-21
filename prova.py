dc5069073b75ca68008be63056eda3d25f3dbb0f
Federico Finco <doppiaeffe94@gmail.com>
2023-12-21 17:44:15 +0100

    dev: all functions done, implemented customizable cols. need to implement custom victory point and add comment

--- prova.py
+++ prova.py
@@ -3,15 +3,43 @@ player=0
 vittoria=4
 colonne=7
 righe=6
-x="ciao"
-print(x)
+victory=""
+ReCCustom=""
+colErr=0
+# SCELTA DELLE GRIGLIE SE MODIFICATE    
+while ReCCustom!="y" and ReCCustom!="n":
+    ReCCustom=str(input("vuoi impostare righe e colonne?\nSe scegli no i valori di defaut sono 7 colonne e 6 righe\n y/n"))
+
+if ReCCustom=="y":
+    colonne=int(input("inserisci il numero di colonne: "))
+    righe=int(input("inserisci il numero di right: "))
+else:
+    print("i valori di default sono stati impostati.")
+
+
+
+
 import numpy as nmp
-m=nmp.array([["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""],["c","c","c","c","c","c","c"]])
+# m=nmp.array([["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""],["c","c","c","c","c","c","c"]])
+d=[]
+for c in range(righe):
+
+    for c in range(colonne):
+        d.append("")
+for c in range(colonne):
+    d.append("c")
+# print(d)
+f=nmp.array(d)
+# print(f)
+m = f.reshape((righe +1), colonne)
+# print(d)
 print(m)
 
+
+
 def read():
-    for c in range(7):
-        for i in range(7):
+    for c in range(righe+1):
+        for i in range(colonne):
             # format per verificare la lettura corretta della matrice
             # if (m[c][i])=="1":
             #     print("|   ", end='')
@@ -33,7 +61,7 @@ def read():
             #         print("  {x} |".format(x=i+1),end='')
             # else :
             #     print("|  o ", end='')
-            if i == 0 and c!=(5+1):
+            if i == 0 and c!=(righe):
                 print("||", end='')
             if (m[c][i])=="":
                 print("   |", end='')
@@ -41,9 +69,9 @@ def read():
                 print(" x |", end='')
             elif (m[c][i])=="o":
                 print(" o |", end='')
-            if i == 6 and c!=(5+1):
+            if i == (colonne-1) and c!=(righe):
                 print("|")
-            elif c==(5+1):
+            elif c==(righe):
                 if i==0:
                     print(" | {x} |".format(x=i+1),end='')
                 else:
@@ -59,13 +87,14 @@ def turnCounter():
     # che brutto pyton devo specificare ogni volta che sto lavorando su una variabile globale bleah
     global player
     if counter%2==0:
-        player=2
+        player="p2"
     else:
-        player=1
+        player="p1"
     print("tocca al giocatore: ",player)
     
 def downControl(y,x):
     # if m[y][x]==m[y-1][x]:
+    global victory
     for c in range(4):
         if m[y][x]==m[y+(c+1)][x] and m[y][x]!="":
             pass
@@ -73,6 +102,7 @@ def downControl(y,x):
             break
         if c==2:
             print("vittoria!")
+            victory=player
 
 def rightControl(y):
     # for c in range(4):
@@ -82,7 +112,7 @@ def rightControl(y):
     #         break
     #     if c==3:
     #         print("vittoria!")
-    
+    global victory
     counter=1
     for c in range((len(m[y]))-1):
         print ("lavoro su",m[y] )
@@ -94,6 +124,7 @@ def rightControl(y):
             # print("azzero il counter",counter,"perchè ",m[y][c],"è diverso da ",m[y][c+1])
         if counter==vittoria:
             print("vittoria")
+            victory=player
 
 def LRCrossConrtol(y,x):
     # counter=0     NON FUNZIONA NEI CASI TIPO TABELLA 4X8
@@ -115,6 +146,7 @@ def LRCrossConrtol(y,x):
     #         # print("azzero il counter",counter,"perchè ",m[y][c],"è diverso da ",m[y][c+1])
     #     if counter==4:
     #             print("vittoria")
+    global victory
     while x!=0 and y!=0:
         c=1
         x=x-c
@@ -124,12 +156,10 @@ def LRCrossConrtol(y,x):
     if y==0:
         if x<=(len(m[0])-1 -vittoria):
             counter=1
-            while y!=righe and x!=colonne:
+            while y!=(righe -1) and (x!=colonne-1):
                 print("lavoro sulla posizione",y,x)
                 c=1
-                x=x+c
-                y=y+c
-                c=c+1
+                
                 
                 if m[y][x]==m[y+1][x+1] and m[y][x]!="":
                     counter=counter+1
@@ -139,12 +169,16 @@ def LRCrossConrtol(y,x):
                     print("azzero il counter",counter,"perchè ",m[y][x],"è diverso da ",m[y+1][x+1])
                 if counter==vittoria:
                     print("vittoria")
+                    victory=player
+                x=x+c
+                y=y+c
+                c=c+1
         else:
             print("il controllo CrossLR non serve perche",x,"è troppo avanti")
     elif x==0:
         if y<=((righe) -(vittoria)):
             counter=1
-            while y!=(righe -1) and x!=(colonne):
+            while y!=(righe -1) and x!=(colonne -1):
                 print("lavoro sulla posizione",y,x)
                 
                 c=1
@@ -162,6 +196,7 @@ def LRCrossConrtol(y,x):
                         print("azzero il counter",counter,"perchè ",m[y][x],"è diverso da ",m[y+1][x+1])
                     if counter==vittoria:
                         print("vittoria")
+                        victory=player
                 else:
                     print("la posizione ",y,x,"è vuota te la stampo :",m[y][x])
                 x=x+c
@@ -173,11 +208,11 @@ def LRCrossConrtol(y,x):
 
 
 def RLCrossConrtol(y,x):
-    
+    global victory
     while x!=colonne-1 and y!=0:
         c=1
         x=x+c
-        y=y+c
+        y=y-c
         c=c+1
     print("sto partendo da:",y,",",x)
     if y==0:
@@ -196,6 +231,7 @@ def RLCrossConrtol(y,x):
                         print("azzero il counter",counter,"perchè ",m[y][x],"è diverso da ",m[y+1][x-1])
                     if counter==vittoria:
                         print("vittoria")
+                        victory=player
                 else:
                     print("la posizione ",y,x,"è vuota te la stampo :",m[y][x])
                 x=x-c
@@ -205,7 +241,7 @@ def RLCrossConrtol(y,x):
             print("il controllo CrossRL non serve perche",x,"è troppo avanti")
             print("il conto viene da questa x in poi ",(vittoria))
     elif x==colonne-1:
-        if y<=((righe) -(vittoria)):
+        if y>=((righe) -(vittoria)):
             counter=1
             while y!=(righe -1) and x!=(colonne):
                 print("lavoro sulla posizione",y,x)
@@ -225,6 +261,7 @@ def RLCrossConrtol(y,x):
                         print("azzero il counter",counter,"perchè ",m[y][x],"è diverso da ",m[y+1][x-1])
                     if counter==vittoria:
                         print("vittoria")
+                        victory=player
                 else:
                     print("la posizione ",y,x,"è vuota te la stampo :",m[y][x])
                 x=x-c
@@ -246,26 +283,35 @@ def RLCrossConrtol(y,x):
 
 
 def placement(col):
-    if player==1:
+    global counter
+    global colErr
+    if player=="p1":
         playersign="x"
-    if player==2:
+    if player=="p2":
         playersign="o"
-    for i in range(6):
-        if (m[i][col-1])=="" and i<=5:
+    
+    if (m[0][col-1])!="":
+        print("la colonna è piena")
+        counter=counter-1
+        colErr=1
+    for i in range(righe):
+        if (m[i][col-1])=="" and i<=(righe-1):
             # print("0")
             # print(i)
-            if i==5 :
+            if i==(righe-1) :
                 (m[i][col-1])=playersign
                 # print("1")
-                # downControl(i,col-1)
-                # rightControl(i)
+                downControl(i,col-1)
+                rightControl(i)
                 LRCrossConrtol(i,col-1)
+                RLCrossConrtol(i,col-1)
             elif (m[i+1][col-1])!="":
                 (m[i][col-1])=playersign
                 # print("2")
-                # downControl(i,col-1)
-                # rightControl(i)
+                downControl(i,col-1)
+                rightControl(i)
                 LRCrossConrtol(i,col-1)
+                RLCrossConrtol(i,col-1)
         else:
             pass
             # print("sono nel pass")
@@ -281,10 +327,28 @@ read()
 # p1ChoosenCol=int(input("\n scegli una colonna:  "))
 # placement(p1ChoosenCol)
 victory=""
-while victory!="p1" or victory!="p2":
+
+while victory!="p1" and victory!="p2":
     # counter=counter+1 risolto con il global nella funzione turncounter
     turnCounter()
     print("conferma turno player",player)
-    p1ChoosenCol=int(input("\n scegli una colonna:  "))
-    placement(p1ChoosenCol)
+    if victory=="p1" or victory=="p2":
+        break
+    check=""
+    while check !="ok":
+        try:
+            p1ChoosenCol=int(input("\n {x}  ".format(x="scegli una colonna" if colErr==0 else "colonna piena/colonna inesistente o non valida")))
+        except ValueError:
+            colErr=1
+            print("non hai scritto un numero")
+        if colErr==0:
+            check="ok"
+    if p1ChoosenCol>(colonne-1):
+        print("la colonna non esiste")
+        counter=counter-1
+        colErr=1
+    else:
+        placement(p1ChoosenCol)
+    colErr=0
     
+print("\n ha vinto il player {x}".format(x=victory))
